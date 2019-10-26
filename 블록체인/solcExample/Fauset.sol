caa2c7f1027b6bb2ec3e1d36aff4597d093e2aae
lacomaco <lacomaco122@gmail.com>
2019-10-26 19:26:40 +0900

    2

--- 블록체인/solcExample/Fauset.sol
+++ 블록체인/solcExample/Fauset.sol
@@ -1,12 +1,38 @@
 pragma solidity ^0.4.26;
-contract Faucet{
 
-    function withdraw(uint withdraw_amount) public{
 
-        require(withdraw_amount <= 100000000);
-
-        msg.sender.transfer(withdraw_amount);
+contract owned{
+    address owner;
+    
+    constructor() {
+        owner=msg.sender;
     }
+    
+    modifier onlyOwner{
+        require(msg.sender==owner,"Only the contract owner can call this function");
+        _;
+    }
+}
 
-    function() external payable{}
+contract mortal is owned{
+    function destroy() public onlyOwner{
+        selfdestruct(owner);
+    }
+}
+
+contract Faucet is mortal{
+    event Withdrawal(address indexed to,uint amount);
+    event Deposit(address indexed from,uint amount);
+    
+    function withdrawa(uint withdraw_amount) public{
+        require(withdraw_amount <= 0.1 ether);
+        require(this.balance >= withdraw_amount,"Insufficient balance in faucet for withdrawal request");
+        
+        msg.sender.transfer(withdraw_amount);
+        emit Withdrawal(msg.sender,withdraw_amount);
+    }
+    
+    function () external payable{
+        emit Deposit(msg.sender,msg.value);
+    }
 }
